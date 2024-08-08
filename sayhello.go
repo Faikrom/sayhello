175c408d4079c2a7ee94cc28204a4c4a94d3e777
Fitroh <pitoananda.ikrom@gmail.com>
2024-08-08 17:34:34 +0700

    membuat module random

--- sayhello.go
+++ sayhello.go
@@ -1,5 +1,18 @@
 package sayhello
 
-func SayHello() string {
+func SayHello(name string, umur int) string {
 	return "Hello World"
+}
+
+func Random(value interface{}) interface{} {
+	switch v := value.(type) {
+	case string:
+		return v + "Hehe"
+	case int:
+		return v + 5
+	case bool:
+		return !v
+	default:
+		return nil
+	}
 }
